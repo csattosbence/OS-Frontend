e26acf08173f060d53125a4a6b228d5036cda18f
Bence <>
2020-12-23 23:47:17 +0100

    First Commit

---
+++ gui/src/Category.js
@@ -0,0 +1,34 @@
+import React, {Component, createContext} from 'react';
+import AppNav from './AppNav';
+
+class Category extends Component {
+    state = {
+        isLoading: true,
+        Categories:[],
+        
+    }
+    async componentDidMount(){
+        const response = await fetch('/api/categories');
+        const body = await response.json();
+        this.setState({Categories: body, isLoading:false});
+    }
+    render(){
+        const {Categories,isLoading} = this.state;
+        if(isLoading)
+            return (<div>isLoading...</div>)
+        return(
+            <div>
+                <AppNav/>
+                <h2>Categories</h2>
+                {
+                    Categories.map( category =>
+                        <div id={category.id}>
+                            {category.name}
+                        </div>
+                    )
+                }
+            </div>
+        );
+    }
+}
+export default Category;
